c535c15d3f2ba33cee83885fe90e7d91f21b0959
Chukwuemeka Kanu <mekuseo@gmail.com>
2023-04-20 14:58:02 +0100

    updated email settings

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,16 +1,37 @@
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 import "../components/ContactFormStyles.css";
 
 function ContactForm() {
+  const form = useRef();
+  const [nameInput, setNameInput] = useState("");
+  const [emailInput, setEmailInput] = useState("");
+  const [messageInput, setMessageInput] = useState("");
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_d20x0ci', 'template_mc3jhfq', form.current, 'Zcp_wyASYiwTqlx1-')
+      .then((result) => {
+          console.log(result.text);
+          console.log("message sent");
+          setNameInput("");
+          setEmailInput("");
+          setMessageInput("");
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
     <>
       <div className="form-container">
-        <h1>Send a message to us!</h1>
-        <form action="send-mail.php" method="POST">
-          <input name="name" placeholder="Name" />
-          <input name="email" placeholder="Email" />
-          <input name="subject" placeholder="Subject" />
-          <textarea name="message" placeholder="Message" rows="4"></textarea>
-          <button>Send Message</button>
+        <h1>Send us a message!</h1>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="user_name" value={nameInput} onChange={(e) => setNameInput(e.target.value)} placeholder="...name" />
+          <input type="email" name="user_email" value={emailInput} onChange={(e) => setEmailInput(e.target.value)} placeholder="...email" />
+          <textarea name="message" value={messageInput} onChange={(e) => setMessageInput(e.target.value)} placeholder="...your message" />
+          <button type="submit" value="Send">SUBMIT</button>
         </form>
       </div>
     </>
