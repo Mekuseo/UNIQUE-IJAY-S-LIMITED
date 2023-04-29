75a9f369bf96d79a251ee951e16ebc35cb4ecedf
Chukwuemeka Kanu <mekuseo@gmail.com>
2023-04-29 16:12:15 +0100

    social media updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,10 +10,10 @@ const Footer = () => {
             <p>Your partner in energy production and exploration.</p>
           </div>
           <div>
-            <a href="https://facebook.com/uniqueijayslimited">
+            <a href="https://web.facebook.com/uniqueijay75?mibextid=ZbWKwL&_rdc=1&_rdr">
               <i className="fa-brands fa-facebook-square"></i>
             </a>
-            <a href="https://instagram.com/uniqueijayslimited">
+            <a href="https://www.instagram.com/uniqueijays_limited/?igshid=ZDdkNTZiNTM%3D">
               <i className="fa-brands fa-instagram-square"></i>
             </a>
           </div>
